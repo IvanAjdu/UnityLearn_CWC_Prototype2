8f14c42f4e606b105924e0b56d2e775a68eed6c3
IvanAjdu <ivan.ajdu@gmail.com>
2024-01-03 00:16:03 +0100

    SpawnManager script with automated spawn interval; change in speed of animals

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalToSpawn;
+    public int zSpawnLoc = 30;
+    private float spawnStart = 2f;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", spawnStart, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalToSpawn.Length);
+        int xSpawnRange = Random.Range(-20, 20);
+        Vector3 xSpawnIndex = new Vector3(xSpawnRange, 0, zSpawnLoc);
+        Instantiate(animalToSpawn[animalIndex], xSpawnIndex, animalToSpawn[animalIndex].transform.rotation);
+    }
+}
